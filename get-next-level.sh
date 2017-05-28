08dd7512b7f0c38f7a14feb7e36eb0021b92e172
lightszentip <lightszentip@gmail.com>
2017-05-28 01:44:26 +0100

    Fix Bugs in script XXX: Major

--- get-next-level.sh
+++ get-next-level.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
-minor=$(git log 0.0.2..HEAD --no-merges --pretty=oneline | grep -c "XXX:.*Minor" 2>&1)
-major=$(git log 0.0.2..HEAD --no-merges --pretty=oneline | grep -c "XXX:.*Major" 2>&1)
 currenttag=$(git describe --tags --dirty --always --long | grep -P '[0-9]+.[0-9]+.[0-9]+' -o 2>&1)
-echo $currenttag
+count_commits=$(git log $currenttag..HEAD --no-merges --pretty=oneline  | grep -c .*[a-zA-Z].* 2>&1)
+minor=$(git log $currenttag..HEAD --no-merges --pretty=oneline | grep -c "XXX:.*Minor" 2>&1)
+major=$(git log $currenttag..HEAD --no-merges --pretty=oneline | grep -c "XXX:.*Major" 2>&1)
+echo "Current version:" $currenttag
+echo "changes:" $count_commits
+if [ $count_commits -eq 0 ]
+then
+	echo "NO changes"
+	exit 0
+fi
+
 version_major=$(echo $currenttag| awk -F \. {'print $1'})
 version_minor=$(echo $currenttag| awk -F \. {'print $2'})
 version_patch=$(echo $currenttag | awk -F \. {'print $3'})
 echo $version_major.$version_minor.$version_patch
 # Patch is by default - no major or minor => patch
 annotated_tag=true
-if [ $major > 0 ]
+if [ $major -gt 0 ]
 then
-   	echo "Major"
+   	echo "Change Major"
 	version_major=$(($version_major+1))
 	version_minor=0
 	version_patch=0
-elif [ $minor > 0 ]
+elif [ $minor -gt 0 ]
 then
-   	echo "Minor"
+   	echo "Change Minor"
 	version_minor=$(($version_minor+1))
 	version_patch=0
 else
-   	echo "Patch"
+   	echo "Change Patch"
 	#for patches only lightweigth
 	annotated_tag=false
 	version_patch=$(($version_patch+1))
 fi
 new_version=$version_major.$version_minor.$version_patch
-echo $new_version
+echo "New Version:" $new_version
 echo "Check exist tag"
 exist_tag=$(git tag | grep -c $new_version 2>&1)
-if [ $exist_tag > 0 ]
+if [ $exist_tag -gt 0 ]
 then
-	echo "Text exist - break"
+	echo "Tag exist - break"
 else
 	if [ $annotated_tag ]
 	then
